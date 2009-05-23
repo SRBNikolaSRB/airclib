b6b66b5aa3f9150f1412cc366d97e281fd4ee6cd
azolex321 <azolex321@dc27d2d0-47e0-11de-988a-a513c5287034>
2009-05-23 21:41:42 +0000

    * Project files , with basic TcpClient Connection handling, example: IrcClient irc As New IrcClient() (IrcClient irc = new IrcClient()) irc.Connect(Server, Port)(;) irc.Connect(Server)(;) etc etc rest can be saw in source, wiki and a lot more coming soon, enjoy!

---
+++ Advanced Irc Lib/Classes/IrcClient.vb
@@ -0,0 +1,120 @@
+﻿' IrcClient.vb
+' Advanced IRC Library Project
+' See LICENSE file for Copyrights
+' Website "http://code.google.com/p/airclib/"
+
+Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.IO
+
+Public Class IrcClient
+    Public WithEvents irc As New TcpClient
+    Public Stream As System.Net.Sockets.NetworkStream
+
+    'Events
+    Public Event OnReciveData(ByVal Data As String)
+    Public Event OnConnect()
+
+    Public isConnected As Boolean
+    Public isInChannel As Boolean
+
+    Public Structure IrcServer
+        Public Server As String
+        Public Port As Integer
+    End Structure
+    Public Structure ChannelMessageData
+        Public Server As String
+        Public Command As String
+        Public Channel As String
+        Public Message As String
+    End Structure
+
+    Public Overloads Sub Connect(ByVal Server As String, ByVal Port As Integer)
+        Try
+            irc.Connect(Server, Port)
+            Stream = irc.GetStream()
+            isConnected = True
+            RaiseEvent OnConnect()
+        Catch e As Exception
+            Return
+        End Try
+    End Sub
+    Public Overloads Sub Connect(ByVal IrcServer As IrcServer)
+        Try
+            irc.Connect(IrcServer.Server, IrcServer.Port)
+            Stream = irc.GetStream()
+            isConnected = True
+            RaiseEvent OnConnect()
+        Catch e As Exception
+            Return
+        End Try
+    End Sub
+
+    Public Function Connected() As Boolean
+        If isConnected = True Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Sub SendData(ByVal Data As String)
+        Dim Writer As New StreamWriter(Stream)
+        Writer.WriteLine(Data, Stream)
+        Writer.Flush()
+    End Sub
+
+    Public Function GetStream() As System.Net.Sockets.NetworkStream
+        Return irc.GetStream()
+    End Function
+
+    Public Sub Listen()
+        If isConnected = False Then
+            Return
+        End If
+
+        Dim Reader As New StreamReader(Stream)
+        Dim Data As String = Reader.ReadLine()
+        While (isConnected = True And Data <> "")
+            RaiseEvent OnReciveData(Data)
+            Listen()
+        End While
+        Reader.Dispose()
+        Data = Nothing
+        Listen()
+    End Sub
+    Public Sub Disconnect()
+        irc.Close()
+    End Sub
+
+    Public Function ReadChannelData(ByVal Data As String) As ChannelMessageData
+        If isInChannel = False Then
+            Return Nothing
+        End If
+
+        Try
+            Dim msg As New ChannelMessageData
+            Dim MessageArray() As String = Split(Data, " ", 4)
+
+            msg.Server = MessageArray(0)
+            msg.Command = MessageArray(1)
+            msg.Channel = MessageArray(2)
+            msg.Message = MessageArray(3)
+
+            Return msg
+        Catch e As Exception
+            Return Nothing
+        End Try
+
+    End Function
+    Public Function ReadMessage(ByVal Data As String) As String
+        If isInChannel = False Then
+            Return Nothing
+        End If
+
+        Dim msg As ChannelMessageData = ReadChannelData(Data)
+        Return msg.Message
+    End Function
+
+End Class
