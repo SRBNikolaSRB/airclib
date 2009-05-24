6b0ec0131b0d061f507b85f7e75d6ae824b15778
azolex321 <azolex321@dc27d2d0-47e0-11de-988a-a513c5287034>
2009-05-24 07:59:37 +0000

    * Public Function InChannel() As Boolean * Public Function GetNick() As String * Nick tracking inside of class * More commenting

--- trunk/Advanced Irc Lib/Classes/IrcClient.vb
+++ trunk/Advanced Irc Lib/Classes/IrcClient.vb
@@ -19,6 +19,7 @@ Public Class IrcClient
     'Class vars
     Public isConnected As Boolean
     Public isInChannel As Boolean
+    Public Nick As String
 
     Public Structure IrcServer
         Public Server As String
@@ -65,6 +66,27 @@ Public Class IrcClient
             Return False
         End If
     End Function
+    '<summary>  
+    'Check's if client is in channel.
+    '</summary>
+    Public Function InChannel() As Boolean
+        If (isConnected = False Or isInChannel = False) Then
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+    '<summary>  
+    'Returns current connection Nick Name
+    '</summary>
+    Public Function GetNick() As String
+        If Nick = Nothing Then
+            Return Nothing
+        Else
+            Return Nick
+        End If
+    End Function
 
     '<summary>  
     'Sends data to currently connected irc server.
@@ -75,10 +97,16 @@ Public Class IrcClient
         Writer.Flush()
     End Sub
 
+    '<summary>  
+    'Returns irc client current stream as System.Net.Sockets.NetworkStream.
+    '</summary>
     Public Function GetStream() As System.Net.Sockets.NetworkStream
         Return irc.GetStream()
     End Function
 
+    '<summary>  
+    'Current connection reads all incoming data, if not null it will start OnReciveData event.
+    '</summary>
     Public Sub Listen()
         If isConnected = False Then
             Return
@@ -106,9 +134,18 @@ Public Class IrcClient
         SendData("JOIN #" & Channel)
         isInChannel = True
     End Sub
+    '<summary>  
+    'Sets current connection Nick Name
+    '</summary>
     Public Sub SetNick(ByVal Nick As String)
-        SendData("NICK " & Nick)
+        Try
+            SendData("NICK " & Nick)
+            Me.Nick = Nick
+        Catch e As Exception
+            Return
+        End Try
     End Sub
+
 
     Public Function ReadChannelData(ByVal Data As String) As ChannelMessageData
         If isInChannel = False Then
