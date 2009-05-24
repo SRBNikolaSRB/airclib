d0897b73308a43921210a7199fca242f3fb18a7d
azolex321 <azolex321@dc27d2d0-47e0-11de-988a-a513c5287034>
2009-05-24 16:45:19 +0000

    * Public Function DoAction(ByVal Channel As String, ByVal Action As String)

--- Advanced Irc Lib/Classes/IrcClient.vb
+++ Advanced Irc Lib/Classes/IrcClient.vb
@@ -254,4 +254,12 @@ Public Class IrcClient
         SendData("WHOIS " & Nick)
         Return Nothing
     End Function
+    Public Function DoAction(ByVal Channel As String, ByVal Action As String)
+        If isConnected = False Or ChannelCount = 0 Then
+            Return Nothing
+        End If
+
+        SendData("PRIVMSG #" & Channel & " :ACTION " & Action & "")
+        Return Nothing
+    End Function
 End Class
