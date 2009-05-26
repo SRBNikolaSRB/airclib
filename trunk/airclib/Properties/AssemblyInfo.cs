2cdd3e6c377a4015577489163a6866078362534b
azolex321 <azolex321@dc27d2d0-47e0-11de-988a-a513c5287034>
2009-05-26 12:07:47 +0000

    * airclib now works for .net 2.0 and so on. * airclib compiled binary release v0.3.0, no release notes for first public binary.

--- trunk/airclib/Properties/AssemblyInfo.cs
+++ trunk/airclib/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("airclib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Advanced Internet Relay Chat Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MILJKOVIC")]
+[assembly: AssemblyCompany("airclib Team")]
 [assembly: AssemblyProduct("airclib")]
-[assembly: AssemblyCopyright("Copyright © MILJKOVIC 2009")]
+[assembly: AssemblyCopyright("Copyright © airclib Team 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
