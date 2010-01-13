314013615e5129995e4131fe6125471a150333c2
azolex321 <azolex321@dc27d2d0-47e0-11de-988a-a513c5287034>
2010-01-13 17:46:02 +0000

    * Removed locals class/namespace * Added airclib.Constants namespace instead moved some vars to IrcClient class

--- trunk/airclib/Code/Locals.cs
+++ trunk/airclib/Code/Locals.cs
@@ -9,83 +9,66 @@ using System.Collections.Generic;
 using System.Text;
 using airclib;
 
-namespace airclib.locals
+namespace airclib.Constants
 {
-    public abstract class Locals
+    public struct IrcServer
     {
-        #region Structures
+        public string Server;
+        public int Port;
+    }
 
-        public struct IrcServer
-        {
-            public string Server;
-            public int Port;
-        }
-        public struct PrivmsgData
-        {
-            public string Sender;
-            public string Command;
-            public string Target;
-            public string Message;
-            public DataType Type;
-            public string WholeData;
-        }
-        public struct ServerData
-        {
-            public string Sender;
-            public string Command;
-            public string Target;
-            public string Message;
-        }
-        public struct ActionData
-        {
-            public string Sender;
-            public string Target;
-            public string Action;
-        }
+    public struct PrivmsgData
+    {
+        public string Sender;
+        public string Command;
+        public string Target;
+        public string Message;
+        public DataType Type;
+        public string WholeData;
+    }
 
-        #endregion
+    public struct ServerData
+    {
+        public string Sender;
+        public string Command;
+        public string Target;
+        public string Message;
+    }
 
-        #region Enums
+    public struct ActionData
+    {
+        public string Sender;
+        public string Target;
+        public string Action;
+    }
 
-        public enum DataType : int
-        {
-            MSGTYPE_USER = 0,
-            MSGTYPE_CHANNEL = 1,
-            MSGTYPE_DEFAULT = 2,
-            MSGTYPE_SERVER = 3,
-            MSGTYPE_ACTION = 4,
-            NULL = 5
-        }
-        public enum ColorMessages : int
-        {
-            Black = 01,
-            Navy_Blue = 02,
-            Green = 03,
-            Red = 04,
-            Brown = 05,
-            Purple = 06,
-            Olive = 07,
-            Yellow = 08,
-            Lime_Green = 09,
-            Teal = 10,
-            Aqua_Light = 11,
-            Royal_Blue = 12,
-            Hot_Pink = 13,
-            Dark_Gray = 14,
-            Light_Gray = 15,
-            White = 16
-        }
+    public enum DataType : int
+    {
+        MSGTYPE_USER = 0,
+        MSGTYPE_CHANNEL = 1,
+        MSGTYPE_DEFAULT = 2,
+        MSGTYPE_SERVER = 3,
+        MSGTYPE_ACTION = 4,
+        NULL = 5
+    }
 
-        #endregion
-
-        #region Text Effects
-
-        public const string BoldFont = "\u0002";
-        public const string ColoredFont = "\u0003";
-        public const string UnderlineFont = "\u001F";
-        public const string FontEnd = "\u000E";
-
-        #endregion
+    public enum ColorMessages : int
+    {
+        Black = 01,
+        Navy_Blue = 02,
+        Green = 03,
+        Red = 04,
+        Brown = 05,
+        Purple = 06,
+        Olive = 07,
+        Yellow = 08,
+        Lime_Green = 09,
+        Teal = 10,
+        Aqua_Light = 11,
+        Royal_Blue = 12,
+        Hot_Pink = 13,
+        Dark_Gray = 14,
+        Light_Gray = 15,
+        White = 16
     }
 }
-
