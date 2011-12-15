1d087bd045f4affa979cbc15306637359cee5f11
milja1337@gmail.com <milja1337@gmail.com@dc27d2d0-47e0-11de-988a-a513c5287034>
2011-12-15 11:04:07 +0000

    * update

--- trunk/src/airclib/Properties/AssemblyInfo.cs
+++ trunk/src/airclib/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Resources;
 // associated with an assembly.
 [assembly: AssemblyTitle("airclib")]
 [assembly: AssemblyDescription("Advanced Internet Relay Chat Library")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("retail")]
 [assembly: AssemblyCompany("Nikola Miljkovic")]
 [assembly: AssemblyProduct("airclib")]
 [assembly: AssemblyCopyright("Copyright © Nikola Miljkovic 2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Leaf")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
